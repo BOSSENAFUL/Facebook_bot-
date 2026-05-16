302153e501a34a2b24c595a9d97f30240c7c3bd8
CYBER BOSS ENAFUL <mondalanful98@gmail.com>
2026-05-17 02:22:03 +0530

    Create index.js

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+/**
+ * CYBERENAFUL MESSENGER BOT - UTILS EXPORTER
+ * Developed by ENAFUL
+ */
+
+const log = require("./log");
+
+// এখানে utils ফোল্ডারের সব মডিউল একসাথে এক্সপোর্ট করা হচ্ছে
+module.exports = {
+  log: log
+};
